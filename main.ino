c0e52ffa1ee294cc586e2196f902c0a12f6d684e
nathanwrightUNR <nathanwright@nevada.unr.edu>
2023-07-27 13:24:20 -0700

    Adding IDLE state logic

--- main.ino
+++ main.ino
@@ -67,7 +67,7 @@ enum State
 
 // Initializing state
 static State currentState = STATE_DISABLED;
-static State previousState = STATE_DISABLED; 
+static State previousState = STATE_DISABLED;
 static bool messagePrinted = false;
 
 // STPMTR
@@ -231,146 +231,254 @@ void loop()
 
     case STATE_IDLE:
     {
-
+        // print state
+        if (currentState != previousState)
+        {
+            U0putstring("State: IDLE\n");
+            displayStateDateAndTime();
+            previousState = currentState;
+        }
+        // turn on green LED
+        *port_h |= (0x1 << 0);
+        // turn OFF button
+        if ((*pin_d & (0x1 << 2)) && (currentState == STATE_IDLE))
+        {
+            currentState = STATE_DISABLED;
+            *port_h &= ~(0x1 << 0);
+        }
+        // monitor temperature and humidity, display on LCD
+        int temperature = dht.readTemperature();
+        int humidity = dht.readHumidity();
+        lcd.setCursor(0, 0);
+        lcd.print("Temperature: ");
+        lcd.print(temperature);
+        lcd.setCursor(0, 1);
+        lcd.print("Humidity: ");
+        lcd.print(humidity);
+        // move vent angle up
+        if (*pin_e & (0x1 << 4))
+        {
+            delay_ms(100);
+            if (*pin_e & (0x1 << 4))
+            {
+                StepsRequired = STEPS_PER_OUT_REV / 16;
+                stepper.setSpeed(100);
+                stepper.step(StepsRequired);
+                U0putstring("Vent moved up by 22.5 degrees\n");
+            }
+        }
+        // move vent angle down
+        else if (*pin_e & (0x1 << 5))
+        {
+            delay_ms(100);
+            if (*pin_e & (0x1 << 5))
+            {
+                StepsRequired = STEPS_PER_OUT_REV / 16;
+                stepper.setSpeed(100);
+                stepper.step(-StepsRequired);
+                U0putstring("Vent moved down by 22.5 degrees\n");
+            }
+        }
+        // monitor water level
+        int adc_value = adc_read(0);
+        if (adc_value < 200)
+        {
+            delay_ms(100);
+            lcd.clear();
+            currentState = STATE_ERROR;
+            return true;
+            break;
+        }
+        // if temp/humidity is above a certain threshold enter RUNNING
+        if (temperature > 26 && (currentState == STATE_IDLE))
+        {
+            currentState = STATE_RUNNING;
+        }
         break;
     }
 
-    case STATE_RUNNING:
+    break;
+    }
+
+case STATE_RUNNING:
+{
+
+    break;
+}
+case STATE_ERROR:
+
+    break;
+}
+
+
+void U0init(int U0baud)
+{
+    unsigned long FCPU = 16000000;
+    unsigned int tbaud;
+    tbaud = (FCPU / 16 / U0baud - 1);
+    *myUCSR0A = 0x20;
+    *myUCSR0B = 0x18;
+    *myUCSR0C = 0x06;
+    *myUBRR0 = tbaud;
+}
+
+unsigned char U0kbhit()
+{
+    return *myUCSR0A & RDA;
+}
+
+unsigned char U0getchar()
+{
+    return *myUDR0;
+}
+
+void U0putstring(char *U0pstring)
+{
+    while (*U0pstring)
     {
-
-        break;
-    }
-    case STATE_ERROR:
-
-        break;
+        while (!(*myUCSR0A & TBE))
+            ;
+        *myUDR0 = *U0pstring;
+        U0pstring++;
     }
 }
 
-void U0init(int U0baud) {
- unsigned long FCPU = 16000000;
- unsigned int tbaud;
- tbaud = (FCPU / 16 / U0baud - 1);
- *myUCSR0A = 0x20;
- *myUCSR0B = 0x18;
- *myUCSR0C = 0x06;
- *myUBRR0  = tbaud;
+void U0putint(int value)
+{
+    char str[12];
+    if (value < 10)
+    {
+        sprintf(str, "0%d", value);
+    }
+    else
+    {
+        sprintf(str, "%d", value);
+    }
+    U0putstring(str);
 }
 
-unsigned char U0kbhit() {
-  return *myUCSR0A & RDA;
+void delay_ms(double delay_time)
+{
+    double clk_period;
+    unsigned int prescaler;
+    // determine which prescaler to use
+    if (delay_time <= 4)
+    {
+        prescaler = 1;
+        clk_period = 0.0000000625;
+    }
+    else if (delay_time <= 33)
+    {
+        prescaler = 8;
+        clk_period = 0.0000000625 * 8;
+    }
+    else if (delay_time <= 262)
+    {
+        prescaler = 64;
+        clk_period = 0.0000000625 * 64;
+    }
+    else if (delay_time <= 1049)
+    {
+        prescaler = 256;
+        clk_period = 0.0000000625 * 256;
+    }
+    else if (delay_time <= 4200)
+    {
+        prescaler = 1024;
+        clk_period = 0.0000000625 * 1024;
+    }
+    else
+    {
+        return;
+    }
+    // caluclate ticks
+    unsigned long ticks = delay_time / 1000 / clk_period;
+    *myTCCR1B &= 0xF8;
+    *myTCNT1 = (unsigned int)(65536 - ticks);
+    *myTCCR1A = 0x0;
+    // set prescale bit
+    switch (prescaler)
+    {
+    case 1:
+        *myTCCR1B |= 0b00000001;
+        break;
+    case 8:
+        *myTCCR1B |= 0b00000010;
+        break;
+    case 64:
+        *myTCCR1B |= 0b00000011;
+        break;
+    case 256:
+        *myTCCR1B |= 0b00000100;
+        break;
+    case 1024:
+        *myTCCR1B |= 0b00000101;
+        break;
+    default:
+        break;
+    }
+    // begin counting
+    while ((*myTIFR1 & 0x01) == 0)
+    {
+    }
+    *myTCCR1B &= 0xF8;
+    *myTIFR1 |= 0x01;
 }
 
-unsigned char U0getchar() {
-  return *myUDR0;
-}
-
-void U0putstring(char* U0pstring) {
-  while(*U0pstring) { 
-      while(!(*myUCSR0A & TBE));
-      *myUDR0 = *U0pstring;
-      U0pstring++;
-  }
-}
-
-void U0putint(int value) {
-  char str[12];
-  if (value < 10) {
-    sprintf(str, "0%d", value);
-  } else {
-    sprintf(str, "%d", value);
-  }
-  U0putstring(str);
-}
-
-void delay_ms(double delay_time) {
-  double clk_period;
-  unsigned int prescaler;
-  // determine which prescaler to use
-  if (delay_time <= 4) {
-    prescaler = 1;
-    clk_period = 0.0000000625;
-  } else if (delay_time <= 33) {
-    prescaler = 8;
-    clk_period = 0.0000000625 * 8;
-  } else if (delay_time <= 262) {
-    prescaler = 64;
-    clk_period = 0.0000000625 * 64;
-  } else if (delay_time <= 1049) {
-    prescaler = 256;
-    clk_period = 0.0000000625 * 256;
-  } else if (delay_time <= 4200) {
-    prescaler = 1024;
-    clk_period = 0.0000000625 * 1024;
-  } else {
-    return;
-  }
-  // caluclate ticks
-  unsigned long ticks = delay_time / 1000 / clk_period;
-  *myTCCR1B &= 0xF8;
-  *myTCNT1 = (unsigned int) (65536 - ticks);
-  *myTCCR1A = 0x0;
-  // set prescale bit
-  switch(prescaler) {
-    case 1:    *myTCCR1B |= 0b00000001; break;
-    case 8:    *myTCCR1B |= 0b00000010; break;
-    case 64:   *myTCCR1B |= 0b00000011; break;
-    case 256:  *myTCCR1B |= 0b00000100; break;
-    case 1024: *myTCCR1B |= 0b00000101; break;
-    default: break;
-  }
-  // begin counting
-  while((*myTIFR1 & 0x01)==0) {}
-  *myTCCR1B &= 0xF8;
-  *myTIFR1 |= 0x01;
-}
-
-void displayStateDateAndTime() {
-  // digital clock display of the time
-  U0putint(hour());
-  printDigits(minute());
-  printDigits(second());
-  U0putstring(" ");
-  U0putint(day());
-  U0putstring(" ");
-  U0putint(month());
-  U0putstring(" ");
-  U0putint(year());
-  U0putstring("\n");
+void displayStateDateAndTime()
+{
+    // digital clock display of the time
+    U0putint(hour());
+    printDigits(minute());
+    printDigits(second());
+    U0putstring(" ");
+    U0putint(day());
+    U0putstring(" ");
+    U0putint(month());
+    U0putstring(" ");
+    U0putint(year());
+    U0putstring("\n");
 }
 
 // used to print colons when necessary
-void printDigits(int digits){
-  U0putstring(":");
-  if(digits < 10) {
-    U0putstring('0');
-  }
-  U0putint(digits);
+void printDigits(int digits)
+{
+    U0putstring(":");
+    if (digits < 10)
+    {
+        U0putstring('0');
+    }
+    U0putint(digits);
 }
 
 // acd init from prev lab
-void adc_init() {
-  *my_ADCSRA |= 0b10000000;
-  *my_ADCSRA &= 0b11011111;
-  *my_ADCSRA &= 0b11110111;
-  *my_ADCSRA &= 0b11111000;
-  *my_ADCSRB &= 0b11110111;
-  *my_ADCSRB &= 0b11111000;
-  *my_ADMUX  &= 0b01111111;
-  *my_ADMUX  |= 0b01000000;
-  *my_ADMUX  &= 0b11011111;
-  *my_ADMUX  &= 0b11100000;
+void adc_init()
+{
+    *my_ADCSRA |= 0b10000000;
+    *my_ADCSRA &= 0b11011111;
+    *my_ADCSRA &= 0b11110111;
+    *my_ADCSRA &= 0b11111000;
+    *my_ADCSRB &= 0b11110111;
+    *my_ADCSRB &= 0b11111000;
+    *my_ADMUX &= 0b01111111;
+    *my_ADMUX |= 0b01000000;
+    *my_ADMUX &= 0b11011111;
+    *my_ADMUX &= 0b11100000;
 }
 
 // adc read function from prev lab
-unsigned int adc_read(unsigned char adc_channel_num) {
-  *my_ADMUX  &= 0b11100000;
-  *my_ADCSRB &= 0b11110111;
-  if(adc_channel_num > 7) {
-    adc_channel_num -= 8;
-    *my_ADCSRB |= 0b00001000;
-  }
-  *my_ADMUX  += adc_channel_num;
-  *my_ADCSRA |= 0x40;
-  while((*my_ADCSRA & 0x40) != 0);
-  return *my_ADC_DATA;
+unsigned int adc_read(unsigned char adc_channel_num)
+{
+    *my_ADMUX &= 0b11100000;
+    *my_ADCSRB &= 0b11110111;
+    if (adc_channel_num > 7)
+    {
+        adc_channel_num -= 8;
+        *my_ADCSRB |= 0b00001000;
+    }
+    *my_ADMUX += adc_channel_num;
+    *my_ADCSRA |= 0x40;
+    while ((*my_ADCSRA & 0x40) != 0)
+        ;
+    return *my_ADC_DATA;
 }
